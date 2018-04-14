9592b963f8010249a9263e8139dac2e7d93c2b8e
Artem3213212 <cx-1212@yandex.ru>
2018-04-14 23:17:42 +0300

    Fix parser

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-import os
+import os,shlex
 from cudatext import *
 
 fn_config = os.path.join(app_path(APP_DIR_SETTINGS), 'cuda_open_url.ini')
@@ -24,25 +24,24 @@ class Command:
         ini_write(fn_config, 'op', 'option_bool', bool_to_str(option_bool))
         file_open(fn_config)
 
-    def call_browser(self,browser,site):
-        if browser[0]=='"':
-          temp==browser.split('"')
-          ss=[temp[1][:-1]]+[temp[2].split()]+[site]
-        else:
-          ss=browser.split()+[site]
+    def call_browser(self, browser, site):
+        ss=shlex.split(browser+' '+site)
         os.spawnv(os.P_NOWAIT+os.P_OVERLAY,ss[0],ss)
 
     def url_chrome(self):
-        self.call_browser('A:\chrome64_49.0.2623.75\chrome.exe','www.foo.com')
+        self.call_browser('chrome.exe','www.foo.com')
 
     def url_chrome_pvt(self):
-        self.call_browser('A:\chrome64_49.0.2623.75\chrome.exe --incognito','www.foo.com')
+        self.call_browser('chrome.exe --incognito','www.foo.com')
 
     def url_firefox(self):
         pass
+        
     def url_firefox_pvt(self):
         pass
+        
     def url_opera(self):
         pass
+        
     def url_opera_pvt(self):
         pass
